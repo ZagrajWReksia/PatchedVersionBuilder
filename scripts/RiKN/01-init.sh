1ab1d61431485af03f54c0695ed2b96d832ce659
Adam Zambrzycki <adam@zambrzycki.net>
2025-11-23 23:58:00 +0100

    Fix building RiKN

--- scripts/RiKN/01-init.sh
+++ scripts/RiKN/01-init.sh
@@ -2,4 +2,4 @@
 set -e
 
 wget -nc -O "$CACHE_DIR/RiKN.iso" "https://iso.zagrajwreksia.pl/RiKN/installed-game-base.zip" || echo "Already downloaded"
-"$COMMON_SCRIPTS_DIR/iso-to-zip.sh" "$CACHE_DIR/RiKN.iso" "$TARGET_ZIP"
+cp "$CACHE_DIR/RiKN.iso" "$TARGET_ZIP"
