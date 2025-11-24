2bba9dc5156e9c34c6f83db723260fb3a200fd3c
Adam Zambrzycki <adam@zambrzycki.net>
2025-11-24 01:46:47 +0100

    Cache RiKN as zip not iso

--- scripts/RiKN/01-init.sh
+++ scripts/RiKN/01-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-wget -nc -O "$CACHE_DIR/RiKN.iso" "https://iso.zagrajwreksia.pl/RiKN/installed-game-base.zip" || echo "Already downloaded"
-cp "$CACHE_DIR/RiKN.iso" "$TARGET_ZIP"
+wget -nc -O "$CACHE_DIR/RiKN.zip" "https://iso.zagrajwreksia.pl/RiKN/installed-game-base.zip" || echo "Already downloaded"
+cp "$CACHE_DIR/RiKN.zip" "$TARGET_ZIP"
