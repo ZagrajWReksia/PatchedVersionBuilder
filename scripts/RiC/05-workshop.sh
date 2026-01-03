518ca68e79bb1c668379f2cb015ab717212be91b
Adam Zambrzycki <adam@zambrzycki.net>
2026-01-03 21:51:00 +0100

    Use ric-workshop fork instead

--- scripts/RiC/05-workshop.sh
+++ scripts/RiC/05-workshop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-wget -nc -O "$CACHE_DIR/ric-workshop-0.5.2.zip" 'https://github.com/roostarreksio/ric-workshop/releases/download/0.5.2/ric-workshop-0.5.2.zip' || echo "Already downloaded"
-zipmerge -I "$TARGET_ZIP" "$TARGET_ZIP" "$CACHE_DIR/ric-workshop-0.5.2.zip"
+wget -nc -O "$CACHE_DIR/ric-workshop-0.1.0.zip" 'https://github.com/ZagrajWReksia/ric-workshop/releases/download/0.1.0/ric-workshop-0.1.0.zip' || echo "Already downloaded"
+zipmerge -I "$TARGET_ZIP" "$TARGET_ZIP" "$CACHE_DIR/ric-workshop-0.1.0.zip"
