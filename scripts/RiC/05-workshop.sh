2601b0e24c5cf29e6d672e649917b19cd44403c5
Adam Zambrzycki <adam@zambrzycki.net>
2026-01-04 04:26:47 +0100

    Update ric-workshop

--- scripts/RiC/05-workshop.sh
+++ scripts/RiC/05-workshop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-wget -nc -O "$CACHE_DIR/ric-workshop-0.1.0.zip" 'https://github.com/ZagrajWReksia/ric-workshop/releases/download/0.1.0/ric-workshop-0.1.0.zip' || echo "Already downloaded"
-zipmerge -I "$TARGET_ZIP" "$TARGET_ZIP" "$CACHE_DIR/ric-workshop-0.1.0.zip"
+wget -nc -O "$CACHE_DIR/ric-workshop-0.2.0.zip" 'https://github.com/ZagrajWReksia/ric-workshop/releases/download/0.2.0/ric-workshop-0.2.0.zip' || echo "Already downloaded"
+zipmerge -I "$TARGET_ZIP" "$TARGET_ZIP" "$CACHE_DIR/ric-workshop-0.2.0.zip"
